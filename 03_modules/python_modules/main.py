f8ff4efa9623e77cf537cd0a46b7beaa2fdc9980
Bjørn <bjand49@gmail.com>
2024-02-14 21:40:06 +0100

    Added 03 excercise

---
+++ 03_modules/python_modules/main.py
@@ -0,0 +1,7 @@
+from my_module import ClassB,ClassA 
+print('Im working wahoooooo')
+print(ClassA)
+ClassA()
+
+print(ClassB)
+ClassB()
---
+++ 03_modules/python_modules/my_module/file_two.py
@@ -0,0 +1,3 @@
+class ClassB:
+    def __init__(self):
+        print('Gotcha class bb u be instantiated')
