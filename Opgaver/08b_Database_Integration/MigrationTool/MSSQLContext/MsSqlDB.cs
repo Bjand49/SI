78671cb5fe638a190dfd8fd11cc242a11b2de0bb
Bjørn <bjand49@gmail.com>
2024-04-27 19:31:36 +0200

    added migration assignment

---
+++ Opgaver/08b_Database_Integration/MigrationTool/MSSQLContext/MsSqlDB.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MigrationTool.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MigrationTool.MSSQLContext
+{
+    public class MsSqlDB : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=127.0.0.1,1433;Database=mssqlmigrate;User Id=sa;Password=yourStrong(!)Password;TrustServerCertificate=True");
+        }
+    }
+}
---
+++ Opgaver/08b_Database_Integration/MigrationTool/MYSQLContext/MySqlDB.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MigrationTool.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MigrationTool.MYSQLContext
+{
+    public class MySqlDB : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySQL("server=127.0.0.1;port=3307;uid=root;pwd=12345;database=mysqlmigration");
+        }
+
+    }
+}
