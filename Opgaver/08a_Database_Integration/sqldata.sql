194cec79ff54fb4811c3019f9c58f89934af0f1a
Bjørn <bjand49@gmail.com>
2024-04-27 18:24:43 +0200

    08a assignment

---
+++ Opgaver/08a_Database_Integration/sqldata.sql
@@ -0,0 +1,107 @@
+-- MySQL dump 10.13  Distrib 8.3.0, for Linux (x86_64)
+--
+-- Host: localhost    Database: carstorage
+-- ------------------------------------------------------
+-- Server version	8.3.0
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `Cars`
+--
+
+DROP TABLE IF EXISTS `Cars`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Cars` (
+  `Id` char(36) NOT NULL,
+  `Name` longtext,
+  `Type` longtext,
+  `Color` longtext,
+  `Battery` longtext,
+  `Hitch` tinyint(1) NOT NULL,
+  `IsAvailable` tinyint(1) NOT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Cars`
+--
+
+LOCK TABLES `Cars` WRITE;
+/*!40000 ALTER TABLE `Cars` DISABLE KEYS */;
+INSERT INTO `Cars` VALUES ('ebe0c0cb-53b4-4466-ba1f-0a2ac1d816e0','Toyota Camry','Sedan','Red','Lithium-ion',0,1);
+/*!40000 ALTER TABLE `Cars` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Part`
+--
+
+DROP TABLE IF EXISTS `Part`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Part` (
+  `Id` char(36) NOT NULL,
+  `Name` longtext,
+  `CarId` char(36) NOT NULL,
+  PRIMARY KEY (`Id`),
+  KEY `IX_Part_CarId` (`CarId`),
+  CONSTRAINT `FK_Part_Cars_CarId` FOREIGN KEY (`CarId`) REFERENCES `Cars` (`Id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Part`
+--
+
+LOCK TABLES `Part` WRITE;
+/*!40000 ALTER TABLE `Part` DISABLE KEYS */;
+INSERT INTO `Part` VALUES ('0bf0efc3-1023-4da0-845d-08fd29b36b94','Transmission','ebe0c0cb-53b4-4466-ba1f-0a2ac1d816e0'),('a5dbd33c-d69c-4dc9-a63b-95a7b65e1e69','Engine','ebe0c0cb-53b4-4466-ba1f-0a2ac1d816e0');
+/*!40000 ALTER TABLE `Part` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `__EFMigrationsHistory`
+--
+
+DROP TABLE IF EXISTS `__EFMigrationsHistory`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `__EFMigrationsHistory` (
+  `MigrationId` varchar(150) NOT NULL,
+  `ProductVersion` varchar(32) NOT NULL,
+  PRIMARY KEY (`MigrationId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `__EFMigrationsHistory`
+--
+
+LOCK TABLES `__EFMigrationsHistory` WRITE;
+/*!40000 ALTER TABLE `__EFMigrationsHistory` DISABLE KEYS */;
+INSERT INTO `__EFMigrationsHistory` VALUES ('20240423130331_init','8.0.4'),('20240423154237_denormalized_scheme','8.0.4'),('20240426092025_furtherDenormalizing','8.0.4');
+/*!40000 ALTER TABLE `__EFMigrationsHistory` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-04-27 16:15:22
